115b66be08c0aa06db4647c1a8909c93fc717bd5
IvanGeffner <ivanegeffner@gmail.com>
2018-02-19 17:43:55 -0500

    added players

---
+++ src/Demo/NullPlayer/UnitPlayer.java
@@ -0,0 +1,19 @@
+package Demo.NullPlayer;
+
+import navgame.*;
+
+public class UnitPlayer {
+
+    public UnitPlayer() {}
+
+    public void run(UnitController uc) throws InterruptedException {
+	/*Insert here the code that should be executed only at the beginning of the unit's lifespan*/
+
+        while (true){
+			/*Insert here the code that should be executed every round*/
+
+            uc.yield(); //End of the turn
+        }
+
+    }
+}
