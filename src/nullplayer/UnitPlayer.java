58542ce96e0320ba5e87481a3df79ca5ef84d30a
IvanGeffner <ivanegeffner@gmail.com>
2018-04-03 11:20:41 -0400

    removed players

---
+++ src/nullplayer/UnitPlayer.java
@@ -0,0 +1,18 @@
+package nullplayer;
+
+import navgame.*;
+
+public class UnitPlayer {
+
+    public void run(UnitController uc) throws InterruptedException {
+	/*Insert here the code that should be executed only at the beginning of the unit's lifespan*/
+
+        while (true){
+			/*Insert here the code that should be executed every round*/
+
+            uc.yield(); //End of turn
+        }
+
+    }
+
+}
